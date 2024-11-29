320cbaaa78baf09ab5750d4e80cc7d7db34dbba5
alfrh02 <alfred.n.hall@gmail.com>
2024-11-29 17:47:27 +0000

    Raylib implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
+#include "raylib.h"
 #include <iostream>
 
 int main() {
-  std::cout << "hello world" << std::endl;
+  InitWindow(800, 450, "raylib [core] example - basic window");
+
+  while (!WindowShouldClose())
+  {
+      BeginDrawing();
+          ClearBackground(RAYWHITE);
+          DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+      EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
 }
